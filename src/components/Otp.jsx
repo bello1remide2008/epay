167425f859779828d64bebee9460fedcd4507d26
Iremide <iremideb82@gmail.com>
2026-03-05 11:38:44 +0100

    Refactor Otp component and update import path

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { saveTransaction } from "./transaction";
+import { saveTransaction } from "./Transaction";
 
 const Otp = () => {
   const navigate = useNavigate();
@@ -60,3 +60,4 @@ const Otp = () => {
 };
 
 export default Otp;
+
